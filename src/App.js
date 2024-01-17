7615601563c0e60cb0f3e97cdf7fbd74423055f9
Jaroslav Hošovský <j.hosovsky@seznam.cz>
2024-01-17 16:52:17 +0100

    First commit - basic structure

--- src/App.js
+++ src/App.js
@@ -1,25 +1,65 @@
-import logo from './logo.svg';
-import './App.css';
+import iconMoon from "./images/icon-moon.svg";
+import { useState, useEffect } from "react";
+// const url = "https://api.github.com/users/mojombo";
 
-function App() {
+const App = () => {
+  // Form
+  const [form, setForm] = useState("");
+
+  // Api
+  const [url, setUrl] = useState("");
+  const [image, setImage] = useState("");
+
+  // Form function
+  const formSubmit = (event) => {
+    event.preventDefault();
+    const apiUrl = `https://api.github.com/users/${form}`;
+    setUrl(apiUrl);
+    setForm("");
+  };
+
+  // Api function
+  const getUser = async (url) => {
+    try {
+      const response = await fetch(url);
+      const data = await response.json();
+      console.log(data);
+      // setName(data[0]?.altSpellings[1]);
+      setImage(data.avatar_url);
+      // setPeople((data[0]?.population / 1000000).toFixed(2));
+    } catch (error) {
+      console.error("Chyba při získávání dat z API", error);
+    }
+  };
+
+  useEffect(() => {
+    if (url) {
+      getUser(url);
+    }
+  }, [url]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <section className="card-wrapper">
+      <div className="header-container">
+        <h2>devfinder</h2>
+        <div className="theme-wrapper">
+          <h5>Dark</h5>
+          <img src={iconMoon} alt="" />
+        </div>
+      </div>
+
+      <form onSubmit={formSubmit}>
+        <img src="" alt="" />
+        <input
+          type="text"
+          placeholder="Search Github username..."
+          value={form}
+          onChange={(event) => setForm(event.target.value)}
+        />
+        <button type="submit">Search</button>
+      </form>
+    </section>
   );
-}
+};
 
 export default App;
