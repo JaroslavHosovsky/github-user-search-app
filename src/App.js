9fdd8170e0905b43574afc461166b723e2e4d0a2
Jaroslav Hošovský <j.hosovsky@seznam.cz>
2024-01-18 20:34:27 +0100

    Sixth commit - Dark mode conditions added

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import iconMoon from "./images/icon-moon.svg";
+import iconSun from "./images/icon-sun.svg";
 import iconSearch from "./images/icon-search.svg";
 import iconLocation from "./images/icon-location.svg";
 import iconWebsite from "./images/icon-website.svg";
@@ -30,6 +31,59 @@ const App = () => {
   // No results
   const [noResults, setNoResults] = useState(false);
   const [placeholder, setPlaceholder] = useState("Search Github username...");
+
+  // Dark mode
+  const [darkMode, setDarkMode] = useState(false);
+
+  const toggleDarkMode = () => {
+    const textDark = document.querySelector(".theme-wrapper h5");
+    const moonIcon = document.querySelector(".theme-wrapper img");
+    const body = document.querySelector("body");
+    const headerH2 = document.querySelector(".header-container h2");
+    const input = document.querySelector("form input");
+    const article = document.querySelector(".info-container");
+    const name = document.querySelector(".main-info div h3");
+    const joined = document.querySelector(".main-info div p:last-of-type");
+    const bio = document.querySelector(".bio");
+    const statisticInfo = document.querySelector(".statistic-info");
+    const headingsH5 = document.querySelector(".statistic-info div h5");
+
+    if (darkMode === false) {
+      textDark.textContent = "Dark";
+      textDark.style.color = "#4b6a9b";
+      body.style.backgroundColor = "#f6f8ff";
+      moonIcon.src = iconMoon;
+      headerH2.style.color = "#222731";
+      input.style.backgroundColor = "#fefefe";
+      input.style.color = "#768fb3";
+      input.style.boxShadow = "0px 5px 15px #a5abc0";
+      article.style.boxShadow = "0px 5px 15px #a5abc0";
+      article.style.backgroundColor = "#fefefe";
+      name.style.color = "#222731";
+      joined.style.color = "#8c9ab1";
+      bio.style.color = "#6580a9";
+      statisticInfo.style.backgroundColor = "#f6f8ff";
+      headingsH5.style.color = "#6580a9";
+    } else {
+      textDark.textContent = "Light";
+      textDark.style.color = "#f2f3f4";
+      body.style.backgroundColor = "#141d2f";
+      moonIcon.src = iconSun;
+      headerH2.style.color = "#f2f3f4";
+      input.style.backgroundColor = "#1e2a47";
+      input.style.color = "#fefefe";
+      input.style.boxShadow = "none";
+      article.style.boxShadow = "none";
+      article.style.backgroundColor = "#1e2a47";
+      name.style.color = "#fefefe";
+      joined.style.color = "#fefefe";
+      bio.style.color = "#fefefe";
+      statisticInfo.style.backgroundColor = "#141d2f";
+      headingsH5.style.color = "#fefefe";
+    }
+
+    setDarkMode(!darkMode);
+  };
 
   // Form function
   const formSubmit = (event) => {
@@ -91,7 +145,7 @@ const App = () => {
     <section className="card-wrapper">
       <div className="header-container">
         <h2>devfinder</h2>
-        <div className="theme-wrapper">
+        <div className="theme-wrapper" onClick={toggleDarkMode}>
           <h5>Dark</h5>
           <img src={iconMoon} alt="" />
         </div>
@@ -119,7 +173,7 @@ const App = () => {
               <p>{"Joined " + created}</p>
             </div>
           </div>
-          <p>
+          <p className="bio">
             {bio ||
               "Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Donec odio. Quisque volutpat mattis eros."}
           </p>
