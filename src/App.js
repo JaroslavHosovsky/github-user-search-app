de3733faf9bff60c526ddab52bfbadbcc265e611
Jaroslav Hošovský <j.hosovsky@seznam.cz>
2024-01-21 11:14:10 +0100

    Seventh commit - Dark mode finished

--- src/App.js
+++ src/App.js
@@ -7,7 +7,6 @@ import iconTwitter from "./images/icon-twitter.svg";
 import iconCompany from "./images/icon-company.svg";
 import { useState, useEffect } from "react";
 import { format } from "date-fns";
-// const url = "https://api.github.com/users/mojombo";
 
 const App = () => {
   // Form
@@ -36,8 +35,7 @@ const App = () => {
   const [darkMode, setDarkMode] = useState(false);
 
   const toggleDarkMode = () => {
-    const textDark = document.querySelector(".theme-wrapper h5");
-    const moonIcon = document.querySelector(".theme-wrapper img");
+    const divTheme = document.querySelector(".header-container div");
     const body = document.querySelector("body");
     const headerH2 = document.querySelector(".header-container h2");
     const input = document.querySelector("form input");
@@ -46,40 +44,94 @@ const App = () => {
     const joined = document.querySelector(".main-info div p:last-of-type");
     const bio = document.querySelector(".bio");
     const statisticInfo = document.querySelector(".statistic-info");
-    const headingsH5 = document.querySelector(".statistic-info div h5");
+    const headingsH5 = document.querySelectorAll(".statistic-info div h5");
+    const images = document.querySelectorAll(".contact-info-img");
+    const paragraphs = document.querySelectorAll(".contact-info div p");
+    const anchorTag = document.querySelector(".contact-info div p a");
+    const numberStats = document.querySelectorAll(".statistic-info div p");
 
     if (darkMode === false) {
-      textDark.textContent = "Dark";
-      textDark.style.color = "#4b6a9b";
+      divTheme.classList.remove("theme-wrapper-dark");
+      divTheme.classList.add("theme-wrapper");
+      if (divTheme.classList.contains("theme-wrapper")) {
+        const textLight = document.querySelector(".theme-wrapper h5");
+        const moonIcon = document.querySelector(".theme-wrapper img");
+        textLight.textContent = "Dark";
+        moonIcon.src = iconMoon;
+      }
+
       body.style.backgroundColor = "#f6f8ff";
-      moonIcon.src = iconMoon;
       headerH2.style.color = "#222731";
       input.style.backgroundColor = "#fefefe";
       input.style.color = "#768fb3";
       input.style.boxShadow = "0px 5px 15px #a5abc0";
+      input.classList.remove("input-dark");
+      input.classList.add("input-light");
+
       article.style.boxShadow = "0px 5px 15px #a5abc0";
       article.style.backgroundColor = "#fefefe";
       name.style.color = "#222731";
       joined.style.color = "#8c9ab1";
       bio.style.color = "#6580a9";
       statisticInfo.style.backgroundColor = "#f6f8ff";
-      headingsH5.style.color = "#6580a9";
+      anchorTag.style.color = "#4b6a9b";
+
+      headingsH5.forEach((heading) => {
+        heading.style.color = "#6580a9";
+      });
+
+      numberStats.forEach((number) => {
+        number.style.color = "#2c3543";
+      });
+
+      images.forEach((image) => {
+        image.style.filter = "none";
+      });
+
+      paragraphs.forEach((paragraph) => {
+        paragraph.style.color = "#4b6a9b";
+      });
     } else {
-      textDark.textContent = "Light";
-      textDark.style.color = "#f2f3f4";
+      divTheme.classList.remove("theme-wrapper");
+      divTheme.classList.add("theme-wrapper-dark");
+      if (divTheme.classList.contains("theme-wrapper-dark")) {
+        const textLight = document.querySelector(".theme-wrapper-dark h5");
+        const sunIcon = document.querySelector(".theme-wrapper-dark img");
+        textLight.textContent = "Light";
+        sunIcon.src = iconSun;
+      }
       body.style.backgroundColor = "#141d2f";
-      moonIcon.src = iconSun;
       headerH2.style.color = "#f2f3f4";
       input.style.backgroundColor = "#1e2a47";
       input.style.color = "#fefefe";
       input.style.boxShadow = "none";
+      input.classList.remove("input-light");
+      input.classList.add("input-dark");
+
       article.style.boxShadow = "none";
       article.style.backgroundColor = "#1e2a47";
       name.style.color = "#fefefe";
       joined.style.color = "#fefefe";
       bio.style.color = "#fefefe";
       statisticInfo.style.backgroundColor = "#141d2f";
-      headingsH5.style.color = "#fefefe";
+      anchorTag.style.color = "#fefefe";
+
+      headingsH5.forEach((heading) => {
+        heading.style.color = "#fefefe";
+      });
+
+      numberStats.forEach((number) => {
+        number.style.color = "#fefefe";
+      });
+
+      images.forEach((image) => {
+        image.style.filter =
+          "brightness(0) saturate(100%) invert(100%) sepia(0%) saturate(1%) hue-rotate(82deg) brightness(109%) contrast(99%)";
+      });
+
+      paragraphs.forEach((paragraph) => {
+        paragraph.style.color = "#fefefe";
+      });
     }
 
     setDarkMode(!darkMode);
@@ -158,6 +210,7 @@ const App = () => {
           placeholder={placeholder}
           value={form}
           onChange={(event) => setForm(event.target.value)}
+          className="input-light"
         />
         {noResults && <p>No results</p>}
         <button type="submit">Search</button>
@@ -196,21 +249,21 @@ const App = () => {
 
         <section className="contact-info">
           <div>
-            <img src={iconLocation} alt="" />
+            <img src={iconLocation} className="contact-info-img" alt="" />
             <p>{location || "Not available"}</p>
           </div>
           <div>
-            <img src={iconWebsite} alt="" />
+            <img src={iconWebsite} className="contact-info-img" alt="" />
             <p>
               <a href={website}>{website || "Not available"}</a>{" "}
             </p>
           </div>
           <div>
-            <img src={iconTwitter} alt="" />
+            <img src={iconTwitter} className="contact-info-img" alt="" />
             <p>{twitter || "Not available"}</p>
           </div>
           <div>
-            <img src={iconCompany} alt="" />
+            <img src={iconCompany} className="contact-info-img" alt="" />
             <p>{company || "Not available"}</p>
           </div>
         </section>
